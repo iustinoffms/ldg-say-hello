0f8e8663517a8378847fff6ac0368b15f1592fcd
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-03-16 17:20:41 +0200

    Adds base files for react sayHello app

---
+++ web-react-ui/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="display"></div>
+      <div className="functionality">
+  
+        <form>
+        <p>cum te cheama boss?</p>
+          <input type="text"></input>
+          <button>Insert</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
