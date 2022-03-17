0962aac456bcd2128857f20d48acd13ecb954161
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-03-17 18:07:35 +0200

    Implements sayHello web-react-UI

--- web-react-ui/src/App.js
+++ web-react-ui/src/App.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  function sayHello(name) {
+    return name === ""
+      ? `Hello, Stranger, nice to meet you!`
+      : `Hello, ${name}, nice to meet you!`;
+  }
+
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName(sayHello(e.target[0].value));
+    e.target[0].value = "";
+  }
+
   return (
     <div className="App">
-      <div className="display"></div>
+      <div className="display" id="display">
+        {name}
+      </div>
       <div className="functionality">
-  
-        <form>
-        <p>cum te cheama boss?</p>
-          <input type="text"></input>
-          <button>Insert</button>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">What is your name ?</label>
+          <input type="text" name="name" id="name"></input>
+          <input type="submit" value="Insert"></input>
         </form>
       </div>
     </div>
