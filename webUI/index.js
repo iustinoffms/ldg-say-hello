aee50516636a347819530048f3c3323abf7a2365
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-03-18 13:18:46 +0200

    Finishes webUI application

--- webUI/index.js
+++ webUI/index.js
@@ -1,17 +1,3 @@
-// const sayHello = require("../domain/sayHello");
-
-function sayHello(name) {
-  const greetings = [
-    `Hello, ${name}, nice to meet you!`,
-    `Greetings, ${name}!`,
-    `Welcome, ${name}! Have a great day`,
-    `Nice to have you, ${name}!`,
-  ];
-
-  if (name === "") return `Hello, Stranger, nice to meet you!`;
-  return greetings[Math.floor(Math.random() * greetings.length)];
-}
-
 const button = document.getElementById("button");
 const inputArea = document.getElementById("name");
 const showGreeting = document.getElementById("show-message");
@@ -20,6 +6,8 @@ const paragraph = document.createElement("p");
 button.addEventListener("click", function (event) {
   event.preventDefault();
   showGreeting.appendChild(paragraph);
-  paragraph.innerHTML = sayHello(inputArea.value);
+  paragraph.innerHTML = sayHello(inputArea.value, () => {
+    return Math.floor(Math.random() * 4);
+  });
   inputArea.value = "";
 });
