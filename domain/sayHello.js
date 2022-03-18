1c1ff1f197d132b31e2d467cac3f9f460b8a6a0f
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-03-18 13:16:50 +0200

    Changes  sayHello functionality

--- domain/sayHello.js
+++ domain/sayHello.js
@@ -1,6 +1,15 @@
-function sayHello(name) {
-  return name === ""
-    ? `Hello, Stranger, nice to meet you!`
-    : `Hello, ${name}, nice to meet you!`;
+function sayHello(name, greetingSelector) {
+  const greetingsMessages = [
+    `Hello, ${name}, nice to meet you!`,
+    `Greetings, ${name}!`,
+    `Welcome, ${name}! Have a great day`,
+    `Nice to have you, ${name}!`,
+  ];
+  if (name === "") return "Hello Stranger";
+
+  const selectedGreet = greetingSelector();
+  if (selectedGreet < 0 || selectedGreet > 3) return name;
+  return greetingsMessages[selectedGreet];
 }
+
 module.exports = sayHello;
--- domain/sayHello.test.js
+++ domain/sayHello.test.js
@@ -1,24 +1,44 @@
 const sayHello = require("./sayHello.js");
 
 describe("sayHello", () => {
-  test("given a name, print Hello name nice to meet you", () => {
-    //Arange
-    const name = "Iustin";
-
-    //Act
-    const result = sayHello(name);
-
-    //Assert
-    expect(result).toEqual("Hello, Iustin, nice to meet you!");
+  test("given a name and a function that returns 0 should return `Hello, name, nice to meet you!`", () => {
+    const result = sayHello("Adi", () => {
+      return 0;
+    });
+    expect(result).toEqual("Hello, Adi, nice to meet you!");
   });
-  test("given no name, print Hello Stranger nice to meet you", () => {
-    //Arange
-    const name = "";
-
-    //Act
-    const result = sayHello(name);
-
-    //Assert
-    expect(result).toEqual("Hello, Stranger, nice to meet you!");
+  test("given a name and function that returns 1 should return `Greetings, name!` ", () => {
+    const result = sayHello("Tibi", () => {
+      return 1;
+    });
+    expect(result).toEqual("Greetings, Tibi!");
+  });
+  test("given a name and function that returns 2 should return `Welcome, name! Have a great day`, ", () => {
+    const result = sayHello("Gabriel", () => {
+      return 2;
+    });
+    expect(result).toEqual(`Welcome, Gabriel! Have a great day`);
+  });
+  test("given a name and function that returns 3 should return `Nice to have you, name!`, ", () => {
+    const result = sayHello("Andreea", () => {
+      return 3;
+    });
+    expect(result).toEqual(`Nice to have you, Andreea!`);
+  });
+  test("given a name and function that returns -1 should return name", () => {
+    const result = sayHello("Bogdan", () => {
+      return -1;
+    });
+    expect(result).toEqual(`Bogdan`);
+  });
+  test("given a name and function that returns 4 should return name", () => {
+    const result = sayHello("Gina", () => {
+      return 4;
+    });
+    expect(result).toEqual(`Gina`);
+  });
+  test("given no name should return Hello Stranger", () => {
+    const result = sayHello("");
+    expect(result).toEqual("Hello Stranger");
   });
 });
